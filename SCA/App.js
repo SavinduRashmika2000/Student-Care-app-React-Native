ecdc5d725082773a44ec77ef36d2c33180ad3ae9
Savindu_Rashmika <savindurashmika2000@gmail.com>
2025-01-06 08:17:35 +0530

    create login page

--- SCA/App.js
+++ SCA/App.js
@@ -1,19 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { Divider, PaperProvider,Text } from 'react-native-paper';
 import { SafeAreaView, ScrollView } from 'react-native-web';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './components/Login';
+import Home from './components/Home';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
    <PaperProvider>
       <SafeAreaView>
-        <ScrollView>
-          <View style={styles.container}>
-            <Text variant="headlineLarge">Headline Large</Text>
-          <StatusBar style="auto" />
-          </View>
-          <Divider/>
-        </ScrollView>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={Login}/>
+            <Stack.Screen name="Home" component={Home}/>
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
     </PaperProvider>
   );
---
+++ SCA/components/Home.js
@@ -0,0 +1,6 @@
+import { Divider, PaperProvider,Text } from 'react-native-paper';
+export default function Home(){
+    return(
+        <Text>aaa</Text>
+    )
+}
